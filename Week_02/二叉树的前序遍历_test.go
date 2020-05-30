4e95fc944cb5d14a6021c8f1c1cc880462f9db9d
zhengqilin <zhengqilin@cmcm.com>
2020-05-30 21:37:03 +0800

    二叉树的前序遍历

--- Week_02/二叉树的前序遍历_test.go
+++ Week_02/二叉树的前序遍历_test.go
@@ -1,11 +1,12 @@
 package Week_02_test
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 // https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+//【题意】
 //给定一个二叉树，返回它的 前序 遍历。
 //
 // 示例:
@@ -19,13 +20,11 @@ import (
 //
 //输出: [1,2,3]
 
-
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
-
 
 /**
  * Definition for a binary tree node.
@@ -35,34 +34,34 @@ type TreeNode struct {
  *     Right *TreeNode
  * }
  */
+//解法【1】
 func preorderTraversal(root *TreeNode) []int {
 
-    res = make([]int, 0)
-    help(root)
-    return res
+	resV2 = make([]int, 0)
+	helpV2(root)
+	return resV2
 }
 
-var res []int
-func help(root *TreeNode)  {
-    if root == nil {
-        return
-    }
-    res = append(res, root.Val)
-    help(root.Left)
-    help(root.Right)
+var resV2 []int
+
+func helpV2(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	resV2 = append(resV2, root.Val)
+	helpV2(root.Left)
+	helpV2(root.Right)
 }
 
-func TestP(t *testing.T)  {
-    tree_node := &TreeNode{
-        Val:1,
-        Right:&TreeNode{
-            Val:2,
-            Left:&TreeNode{
-                Val:3,
-            },
-        },
-    }
-    fmt.Println(preorderTraversal(tree_node))
+func TestP(t *testing.T) {
+	tree_node := &TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+	fmt.Println(preorderTraversal(tree_node))
 }
-
-
