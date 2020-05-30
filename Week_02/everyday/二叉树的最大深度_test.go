e97df8830f2b3219d8cdb1ede030d0fa3cb9a56c
zhengqilin <zhengqilin@cmcm.com>
2020-05-30 23:16:37 +0800

    二叉树最大深度

---
+++ Week_02/everyday/二叉树的最大深度_test.go
@@ -0,0 +1,62 @@
+package Week_02_everyday
+
+import (
+	"fmt"
+	"testing"
+)
+
+//https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+//
+//给定一个二叉树，找出其最大深度。
+//
+//二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+//说明: 叶子节点是指没有子节点的节点。
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
+
+func TestMaxDe(t *testing.T) {
+	tree_node := &TreeNode{
+		Val: 3,
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+		Left: &TreeNode{
+			Val: 9,
+		},
+	}
+	fmt.Println(maxDepth(tree_node))
+}
